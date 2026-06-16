acef810aa527922df5de0430149bfbcca2472f54
Gourav554 <jgourav554@gmail.com>
2026-06-16 23:05:30 +0530

    String

---
+++ JavaScript/String.js
@@ -0,0 +1,45 @@
+//Length
+//string ki size Batayega
+let name  = "Gourav";
+console.log(name.length); //7
+
+//toUpperCase
+//string ko capital me convert kar dega
+let name1 = "gourav";
+console.log(name1.toUpperCase()); 
+
+//toLowerCase
+//string ko small me convert kar dega
+let name2 = "GOURAV";
+console.log(name2.toLowerCase()); 
+
+//Trim
+//space ko Trim karta hai Left or right
+let name2 = "           GOURAV";
+console.log (name2.trim());
+
+//endswitch
+
+
+
+
+
+//indexof
+
+
+
+
+
+//lastindexof
+//Piche se uska Index Dekhne lagega
+
+console.log(e.indexof("asdfghjkl"));
+
+
+//Slice
+
+
+
+
+
+//
