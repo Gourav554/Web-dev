9ce146351c9742c6767898e6bd8dd78ddd2bd18a
Gourav554 <Jgourav554@gmail.com>
2026-08-17 22:31:20 +0530

    closer

--- JavaScript/Closer.js
+++ JavaScript/Closer.js
@@ -3,27 +3,23 @@ let array = ["apple", "banana", "orange", "grape", "mango"];
 // using Array constructor
 let fruits = new Array("apple", "banana", "orange", "grape", "mango", "lichi");
 console.log(array.length);
-// push 
 // array ke ander element add karta hai
 array.push("watermelon");
 console.log(array);
 
-//pop  
+  
 // element remove kar dega
 array.pop("watermelon");
 console.log(array);
 
-//unshift
 //element ko starting mai dal dega
 array.unshift("watermelon");
 console.log(array);
 
-//shift
 //staring index remove kar dega 
 array.shift("watermelon");
 console.log(array);
 
-//includes
 // check kart hai ki element  present hai ya nahi 
 console.log(array.includes("mango"))
 console.log(array.includes("watermelon"))
@@ -37,10 +33,9 @@ console.log(array.join("-"))
     //elements bataiga os ange ke 
 console.log(array.slice(1, 3));
 
-// splice
 // elements bataiga uss range ki right bound tak 
 console.log(array.splice(1, 4));
-//normal loop
+
 array = ["apple", "banana", "orange", "grape", "mango"];
 for (var i = 0; i < array.length; i++) {
     console.log(array[i]);
